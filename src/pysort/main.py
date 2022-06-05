2dc367d8e9fa94e9a4d819385c4409d31355a5c5
RESPULTE <yeapzing@gmail.com>
2022-06-06 01:26:50 +0800

    created a functional display for the sorting algorithm

--- src/pysort/main.py
+++ src/pysort/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 from itertools import zip_longest
 from pysort.lib import *
 
@@ -6,15 +7,18 @@ import pygame
 import random
 
 
+pygame.init()
+pygame.font.init()
+
+
 class Settings:
     FPS = 60
-    PADDING = 15
+    PADDING = 5
 
     SCREEN_WIDTH = 1000
     SCREEN_HEIGHT = 700
 
-    FONT = "Comic Sans MS"
-    FONT_SIZE = 25
+    FONT = pygame.font.SysFont("Arial", 25)
     FONT_DEFAULT_COLOR = (255, 0, 255)
     FONT_HOVERED_COLOR = (0, 255, 255)
     FONT_CLICKED_COLOR = (0, 0, 0)
@@ -29,6 +33,10 @@ class Settings:
     SORTING_DISPLAYER_COLUMN_COLOR_INITIAL = (13, 53, 156)
     SORTING_DISPLAYER_COLUMN_COLOR_END = (130, 200, 200)
 
+    TITLE_RECT = pygame.Rect(100, 20, 800, 65)
+    TITLE_FONT_COLOR = (0, 0, 100)
+    BACKGROUND_COLOR = (255, 255, 255)
+
 
 class Button(pygame.Surface):
     def __init__(
@@ -42,20 +50,14 @@ class Button(pygame.Surface):
         func: Callable = None,
     ) -> None:
         super().__init__((w, h))
-        self.default_color = Settings.BUTTON_DEAFULT_COLOR
-        self.hovered_color = Settings.BUTTON_HOVERED_COLOR
-        self.clicked_color = Settings.BUTTON_CLICKED_COLOR
-        self._current_color = Settings.BUTTON_DEAFULT_COLOR
 
         self.text = text
-        self.font = pygame.font.SysFont(Settings.FONT, Settings.FONT_SIZE)
-        self.default_font_color = Settings.FONT_DEFAULT_COLOR
-        self.clicked_font_color = Settings.FONT_CLICKED_COLOR
-        self.hovered_font_color = Settings.FONT_HOVERED_COLOR
-        self._current_font_color = Settings.FONT_DEFAULT_COLOR
+        self.func = func
 
         self.relative_pos = (x, y)
         self.container = container
+        self._current_color = Settings.BUTTON_DEAFULT_COLOR
+        self._current_font_color = Settings.FONT_DEFAULT_COLOR
 
         self.render()
         self.container.blit(self, (x, y))
@@ -63,48 +65,43 @@ class Button(pygame.Surface):
 
     def render(self) -> None:
         self.fill(self._current_color)
-        font_surface = self.font.render(self.text, True, self._current_font_color)
+        font_surface = Settings.FONT.render(self.text, True, self._current_font_color)
         self.blit(font_surface, font_surface.get_rect(center=self.get_rect().center))
 
     def set_hovered(self) -> None:
-        self._current_color = self.hovered_color
-        self._current_font_color = self.hovered_font_color
-        self.container.hovered_button = self
+        self._current_color = Settings.BUTTON_HOVERED_COLOR
+        self._current_font_color = Settings.FONT_HOVERED_COLOR
 
         self.render()
         self.container.blit(self, self.relative_pos)
 
     def set_clicked(self) -> None:
-        self._current_color = self.clicked_color
-        self._current_font_color = self.clicked_font_color
-        self.container.clicked_button = self
-        self.container.hovered_button = None
+        self._current_color = Settings.BUTTON_CLICKED_COLOR
+        self._current_font_color = Settings.FONT_CLICKED_COLOR
 
         self.render()
         self.container.blit(self, self.relative_pos)
 
     def set_default(self) -> None:
-        self._current_color = self.default_color
-        self._current_font_color = self.default_font_color
+        self._current_color = Settings.BUTTON_DEAFULT_COLOR
+        self._current_font_color = Settings.FONT_DEFAULT_COLOR
 
         self.render()
         self.container.blit(self, self.relative_pos)
 
 
 class DropDownMenu(pygame.Surface):
-    clicked_ms: int = 150
-
-    def __init__(self, rect: pygame.Rect, text_list: List[str], func_list: List[Callable], **kwargs) -> None:
-        x, y, w, h = rect
-        self.x, self.y = x, y
-
-        total_btn = len(func_list)
-        btn_h = (h - (Settings.PADDING * (total_btn + 1))) // total_btn
-        super().__init__((w, h))
+    def __init__(self, text_list: List[str], func_list: List[Callable], container: pygame.Surface, **kwargs) -> None:
+        self.container = container
+        self.x, self.y, self.w, self.h = Settings.SORTING_ALGORITHM_MENU_RECT
+        super().__init__((self.w, self.h))
 
         self.buttons: List[Button] = []
+
+        total_btn = len(func_list)
+        btn_h = (self.h - (Settings.PADDING * (total_btn + 0.5))) // total_btn
         btn_x = btn_y = Settings.PADDING
-        btn_w = w - Settings.PADDING * 2
+        btn_w = self.w - Settings.PADDING * 2
         for text, fn in zip_longest(text_list, func_list, fillvalue=None):
             btn = Button(btn_x, btn_y, btn_w, btn_h, text=text, container=self, func=fn, **kwargs)
             btn_y += btn_h + Settings.PADDING
@@ -113,7 +110,11 @@ class DropDownMenu(pygame.Surface):
         self.clicked_button: Button = None
         self.hovered_button: Button = None
 
+        self.container.blit(self, (self.x, self.y))
+
     def query(self, x: int, y: int, clicked: bool = False) -> None:
+        clicked_btn_func = None
+
         for btn in self.buttons:
 
             if btn is self.clicked_button:
@@ -122,18 +123,42 @@ class DropDownMenu(pygame.Surface):
             if not btn.absolute_rect.collidepoint(x, y):
                 if btn is self.hovered_button:
                     btn.set_default()
-                continue
+                    self.hovered_button = None
 
-            if clicked:
+            elif clicked:
                 if self.clicked_button != None:
                     self.clicked_button.set_default()
+
                 btn.set_clicked()
-                return btn
+                self.clicked_button = btn
+                self.hovered_button = None
+
+                clicked_btn_func = btn.func
 
             elif btn is not self.hovered_button:
                 if self.hovered_button != None:
                     self.hovered_button.set_default()
+
                 btn.set_hovered()
+                self.hovered_button = btn
+
+            self.container.blit(self, (self.x, self.y))
+
+        return clicked_btn_func
+
+
+class Title(pygame.Surface):
+    def __init__(self, container: pygame.Surface) -> None:
+        self.x, self.y, self.w, self.h = Settings.TITLE_RECT
+        super().__init__((self.w, self.h))
+        self.container = container
+        self.fill(Settings.BACKGROUND_COLOR)
+
+    def set(self, text: str) -> None:
+        self.fill(Settings.BACKGROUND_COLOR)
+        font_surface = Settings.FONT.render("SELECTED: " + text, True, Settings.TITLE_FONT_COLOR)
+        self.blit(font_surface, (self.x, (self.h - self.y) // 2))
+        self.container.blit(self, (self.x, self.y))
 
 
 class SortingDisplayer(pygame.Surface):
@@ -143,15 +168,36 @@ class SortingDisplayer(pygame.Surface):
 
         self.dataset = []
         self.dataset_size = 0
-        self.sort_algo = None
+        self.sort_algo: Callable[[MutableSequence[CT]], Iterable] = None
         self.container = container
 
         self.generate_data(100)
         self.render()
 
     def start(self) -> None:
-        if self.sort_algo is None:
-            raise Exception
+        sorter = self.sort_algo(self.dataset)
+        clock = pygame.time.Clock()
+
+        run = True
+        while run:
+            clock.tick(Settings.FPS)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        exhaust(sorter)
+
+            try:
+                next(sorter)
+                self.render()
+                pygame.display.update(Settings.SORTING_DISPLAYER_RECT)
+
+            except StopIteration:
+                self.render()
+                pygame.display.update(Settings.SORTING_DISPLAYER_RECT)
+                break
 
     def generate_data(self, n: int) -> List[int]:
         self.dataset_size = n
@@ -159,6 +205,7 @@ class SortingDisplayer(pygame.Surface):
         self.dataset.extend([random.randint(1, n) for _ in range(1, n + 1)])
 
     def render(self) -> None:
+        self.fill((0, 0, 0))
         ir, ig, ib = Settings.SORTING_DISPLAYER_COLUMN_COLOR_INITIAL
         er, eg, eb = Settings.SORTING_DISPLAYER_COLUMN_COLOR_END
         dr, dg, db = er - ir, eg - ig, eb - ib
@@ -177,14 +224,21 @@ class SortingDisplayer(pygame.Surface):
             pygame.draw.rect(self, color, (x, y, w, h))
             x += w
 
+        self.container.blit(self, (self.x, self.y))
+
     def set(self, sort_algo: Callable) -> None:
         self.sort_algo = sort_algo
+
+
+def exhaust(iterable: Iterable) -> None:
+    deque(iterable, 0)
 
 
 def main():
     pygame.init()
     pygame.font.init()
     screen = pygame.display.set_mode((Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT))
+    screen.fill(Settings.BACKGROUND_COLOR)
 
     dataset_size = {
         10: "teeny-weeny",
@@ -194,20 +248,22 @@ def main():
         100_000: "humungous",
         1_000_000: "oh lawd",
     }
-    sorting_algorithms = [bubble_sort, insertion_sort, selection_sort]
+    sorting_algorithms = [bubble_sort, insertion_sort, selection_sort, merge_sort]
+    for algo in sorting_algorithms:
+        algo.__name__ = algo.__name__.replace("_", " ").upper()
+
     sorting_algorithm_names = [fn.__name__ for fn in sorting_algorithms]
-    menu = DropDownMenu(
-        rect=Settings.SORTING_ALGORITHM_MENU_RECT, text_list=sorting_algorithm_names, func_list=sorting_algorithms
-    )
+
+    menu = DropDownMenu(text_list=sorting_algorithm_names, func_list=sorting_algorithms, container=screen)
     displayer = SortingDisplayer(container=screen)
+    title = Title(container=screen)
 
     running = True
     start_sort = False
+    screen.blit(title, (title.x, title.y))
     clock = pygame.time.Clock()
     while running:
-        dt = clock.tick(Settings.FPS)
-
-        screen.fill((255, 255, 255))
+        clock.tick(Settings.FPS)
 
         mouse_pos = pygame.mouse.get_pos()
         clicked = False
@@ -222,12 +278,14 @@ def main():
                 if event.key == pygame.K_RETURN:
                     start_sort = True
 
-        screen.blit(menu, (menu.x, menu.y))
-        screen.blit(displayer, (displayer.x, displayer.y))
-
-        sort_algo = menu.query(*mouse_pos, clicked=clicked)
-        if sort_algo != None:
-            displayer.set(sort_algo)
+        if not start_sort:
+            sort_algo = menu.query(*mouse_pos, clicked=clicked)
+            if sort_algo != None:
+                displayer.set(sort_algo)
+                title.set(sort_algo.__name__)
+        else:
+            displayer.start()
+            start_sort = False
 
         pygame.display.flip()
 
